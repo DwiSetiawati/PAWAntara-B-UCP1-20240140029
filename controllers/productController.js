80d6ce67491d743f441f3f50364140149d0f13b8
DwiSetiawati <dwisetwa27@gmail.com>
2026-08-11 10:00:00 +0700

    Sprint2 - feat: Implement getAllProducts function to retrieve products based on category and search query

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,1 +1,11 @@
 const productModel = require("../models/productModel");
+
+function getAllProducts(req, res) {
+  const { kategori, search } = req.query;
+  const data = productModel.getAll({ kategori, search });
+
+  res.status(200).json({
+    status: "success",
+    data,
+  });
+}
