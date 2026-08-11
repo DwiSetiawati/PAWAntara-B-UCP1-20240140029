442d33aef550fe03d70ca870a4cd38387d54e6b0
DwiSetiawati <dwisetwa27@gmail.com>
2026-08-11 10:00:00 +0700

    Sprint2 - feat: Expand chat rules in chatController for enhanced customer interaction

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,5 +1,31 @@
 const productModel = require("../models/productModel");
 
 const rules = [
-
+  {
+    keywords: ["jam buka", "buka jam", "jam operasional", "tutup jam"],
+    reply: "Toko kami buka setiap hari jam 07.00 - 20.00!",
+  },
+  {
+    keywords: ["ongkir", "antar", "diantar", "kirim ke rumah", "delivery"],
+    reply:
+      "Bisa antar untuk area sekitar toko. Ongkir menyesuaikan jarak, silakan konfirmasi alamat lewat WhatsApp toko ya!",
+  },
+  {
+    keywords: ["bayar", "pembayaran", "transfer", "cod", "qris"],
+    reply:
+      "Pembayaran bisa lewat cash (COD), transfer bank, atau QRIS. Fleksibel kok!",
+  },
+  {
+    keywords: ["stok", "tersedia", "ada ga", "ada gak", "ready"],
+    reply:
+      "Untuk cek stok produk tertentu, coba kunjungi halaman Produk kami ya, datanya selalu ter-update!",
+  },
+  {
+    keywords: ["halo", "hai", "hi", "pagi", "siang", "malam"],
+    reply: "Halo juga! Ada yang bisa saya bantu seputar produk toko kami?",
+  },
+  {
+    keywords: ["makasih", "terima kasih", "thanks"],
+    reply: "Sama-sama! Senang bisa membantu.",
+  },
 ];
