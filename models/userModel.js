bad6006f840bac5ea3683ae48664970ebb669fcb
DwiSetiawati <dwisetwa27@gmail.com>
2026-08-11 10:00:00 +0700

    Sprint2 - chore: Refactor user model to include password validation logic

--- models/userModel.js
+++ models/userModel.js
@@ -2,3 +2,8 @@ const bcrypt = require("bcryptjs");
 
 const ADMIN_USERNAME = process.env.ADMIN_USERNAME;
 const ADMIN_PASSWORD_HASH = process.env.ADMIN_PASSWORD_HASH;
+
+function validateCredentials(username, password) {
+  if (username !== ADMIN_USERNAME) return false;
+  return bcrypt.compareSync(password, ADMIN_PASSWORD_HASH);
+}
