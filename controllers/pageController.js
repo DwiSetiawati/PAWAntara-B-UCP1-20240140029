b82d2957f9b178ab3ed952a827a16601dd4598ef
DwiSetiawati <dwisetwa27@gmail.com>
2026-08-11 10:00:00 +0700

    Sprint2 - feat: Implement renderBeranda function to display preview products on the homepage

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,1 +1,9 @@
 const productModel = require("../models/productModel");
+
+function renderBeranda(req, res) {
+  const previewProducts = productModel.getAll().slice(0, 3);
+  res.render("index", {
+    title: "Beranda - Toko Sembako Ariesta",
+    previewProducts,
+  });
+}
